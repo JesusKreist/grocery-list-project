37cbf9f5a38b87db82aa142466659abbe0b535e4
Samuel Ude <59575776+JesusKreist@users.noreply.github.com>
2020-07-21 17:17:20 +0100

    Finished project for now.

--- js/app.js
+++ js/app.js
@@ -1,124 +1,111 @@
-//Get five elements and assign them to variables.
+const items = document.querySelector(".list-items");
+const submitFormButton = document.querySelector(".submitBtn");
+const inputField = document.getElementById("input-value");
+const clearButton = document.querySelector(".clearBtn");
+const feedback = document.querySelector(".feedback");
 
+Storage.prototype.setObj = function (key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function (key) {
+    return JSON.parse(this.getItem(key))
+}
 
-const form = document.getElementById('input-form');
-const input = document.getElementById('input-value');
-const feedback = document.querySelector('.feedback');
-const listItems = document.querySelector('.list-items');
-const clearBtn = document.querySelector('.clearBtn');
+let groceryList;
+let state;
 
-//add event listeners
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    const value = input.value;
+// // add items from the localStorage to the groceryItems array
+localStorage.getObj("groceryItems") ? groceryList = localStorage.getObj("groceryItems") : groceryList = {};
+localStorage.getObj("counter") ? counter = localStorage.getObj("counter") : counter = 0;
 
-    //check if value is empty
-
-    if(value === ''){
-        const result = 'alert-danger';
-        const text = 'Can not add empty value!';
-        showFeedback(feedback, text, result);
-    } else{
-        //add to list
-        addItem(value);
-        //add to storage
-        addStorage(value);
-    }
-});
-
-//clear btn event listener
-clearBtn.addEventListener('click', function(){
-    while(listItems.children.length>0){
-        listItems.removeChild(listItems.children[0]);
-        //clear storage
-        clearStorage();
-    }
-});
-
-//delete one item
-listItems.addEventListener('click', function(event){
-    if(event.target.parentElement.classList.contains('remove-icon')){
-        let parent = event.target.parentElement.parentElement;
-        listItems.removeChild(parent);
-        let text = event.target.parentElement.previousElementSibling.textContent;
-        clearSingle(text);
-    }
-})
-//dom content loaded
-
-document.addEventListener('DOMContentLoaded', function(){
-    loadItems();
-})
-//functions
-//show feedback
-function showFeedback(element, text, result){
-    element.classList.add('showItem',`${result}`);
-    element.innerHTML = `<p>${text}</p>`;
-
-    setTimeout(function(){
-        element.classList.remove('showItem');
-    }, 3000)
-   
+// add the html stored in the values of the groceryList object to the document body.
+for (let i of Object.values(groceryList)) {
+    items.insertAdjacentHTML("beforebegin", i);
 };
 
-//add an item
-function addItem(value){
-    const div = document.createElement('div');
-    div.classList.add('item', 'my-3', 'd-flex','justify-content-between','p-2');
-    div.innerHTML = `<h5 class="item-title text-capitalize">${value}</h5>
-    <span class="remove-icon text-danger"><i class="fas fa-trash"></i></span>
-   </div>`
-   listItems.appendChild(div);
-   input.value = '';
-   showFeedback(feedback, 'item added to the list', 'alert-success');
+
+
+// // clear the local storage, loop through all the keys in the groceryList == the div ids
+// // get the elements and delete them
+// // set the groceryList back to an empty object
+
+const clearAll = () => {
+    localStorage.clear();
+    for (let i of Object.keys(groceryList)) {
+        document.getElementById(i).remove();
+    };
+    groceryList = {};
 };
 
-//add to local storage
-function addStorage(value){
-    let items;
 
-    if(localStorage.getItem('grocery-list')){
-        items = JSON.parse(localStorage.getItem('grocery-list'));
+// // get the event that triggered the action and go high-up the hierachy
+const deleteEntry = event => {
+    let entry = event.target.parentNode.parentNode;
+    entry.remove();
+    delete groceryList[entry.id];
+    localStorage.setObj("groceryItems", groceryList);
+};
+
+
+
+// loop through the keys of the groceryList object and wire up event listeners for their div entries
+for (let e of Object.keys(groceryList)) {
+    const deleteBtn = document.getElementById(e).querySelector(".remove-icon");
+    deleteBtn.addEventListener("click", deleteEntry);
+};
+
+
+const addItem = () => {
+    let newEntry = inputField.value;
+    // check if new entry is not empty
+    // add a check to see if the element is already in the array
+    if (newEntry) {
+        let itemId = `item${counter}`;
+        let newItem = `<div id=${itemId} class="item my-3 d-flex justify-content-between p-2">
+       <h5 class="item-title text-capitalize">${newEntry}</h5>
+       <span class="remove-icon text-danger"><i class="fas fa-trash"></i></span>
+      </div>`
+        // increment counter, add the new id to the groceryList object, add the on state to the state object
+        // reset the input filed value
+        counter++
+        groceryList[itemId] = newItem;
+        inputField.value = "";
+        // add the items to the local storage
+        localStorage.setObj("groceryItems", groceryList);
+        localStorage.setObj("counter", counter);
+        // add the new html to the DOM 
+        items.insertAdjacentHTML("beforebegin", newItem);
+        // get the new buttons from the newly inserted html and wire up the event listeners
+        const deleteBtn = document.getElementById(itemId).querySelector(".remove-icon");
+        console.log(deleteBtn);
+        deleteBtn.addEventListener("click", deleteEntry);
+        feedback.innerHTML = "Item was successfully added to the list"
+        setTimeout(() => {
+            feedback.style.display = "none";
+            feedback.classList.remove("showItem", "alert-success", "text-capitalize");
+        }, 3500);
+        feedback.style.display = "block";
+        feedback.classList.add("showItem", "alert-success", "text-capitalize");
+
     } else {
-        items = [];
-    }
-
-    items.push(value);
-    localStorage.setItem('grocery-list', JSON.stringify(items));
-
+        feedback.innerHTML = "Please add an item to your list!"
+        setTimeout(() => {
+            feedback.style.display = "none";
+            feedback.classList.remove("showItem", "alert-danger", "text-capitalize");
+        }, 3500);
+        feedback.style.display = "block";
+        feedback.classList.add("showItem", "alert-danger", "text-capitalize");
+    };
 };
 
-//clear local storage
-function clearStorage(){
-   localStorage.removeItem('grocery-list');
-};
 
-//clear single item in the local storage
-function clearSingle(value){
-    const tempItems = JSON.parse(localStorage.getItem('grocery-list'));
-    const items = tempItems.filter(function(item){
-        if(item !== value){
-            return item;
-        }
-    })
-    localStorage.removeItem('grocery-list');
-    localStorage.setItem('grocery-list', JSON.stringify(items));
+const testSubmit = event => {
+    event.preventDefault();
+    addItem();
 }
 
-//load items
-function loadItems(){
 
-    if(localStorage.getItem('grocery-list')){
-        const items = JSON.parse(localStorage.getItem('grocery-list'))
+submitFormButton.addEventListener("click", testSubmit);
+clearButton.addEventListener("click", clearAll);
 
-        items.forEach(function(item){
-            const div = document.createElement('div');
-    div.classList.add('item', 'my-3', 'd-flex','justify-content-between','p-2');
-    div.innerHTML = `<h5 class="item-title text-capitalize">${item}</h5>
-    <span class="remove-icon text-danger"><i class="fas fa-trash"></i></span>
-   </div>`
-   listItems.appendChild(div);
-        })
-    }
-    
-}
+
